72279723b99450913a524ee0bffe2d66524a1f4b
szw1223 <47969543+szw1223@users.noreply.github.com>
2020-02-17 20:09:44 -0500

    Add files via upload

---
+++ dynamic_programming.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def pt(n):
+    p = [0,1,5,8,9,10,17,17,20,24,30]
+    p = p + [0] * (n - len(p) + 1)
+    pf = [0] * (n + 1)
+    
+    for i in range(1, n + 1):
+        
+        if n == 0:
+            return 0
+            
+        pcurrent = []
+        for j in range(1, (i + 1)):
+            pcurrent.append(p[j] + pf[i - j])
+        pf[i] = max(pcurrent)
+        
+    return pf[n]
+    
+print(pt(150))
+
+
+def pt_1(n):
+    p = [0,1,5,8,9,10,17,17,20,24,30]
+    p = p + [0] * (n - len(p) + 1)
+    pf = [0] * (n + 1)
+    
+    
+def cut(n):
+    p = [0,1,5,8,9,10,17,17,20,24,30]
+    p = p + [0] * (n - len(p) + 1)
+    pf = [0] * (n + 1)
+    if n == 0:
+        return 0
+    q = -1000
+    for i in range(1, n + 1):
+        q = max(q, p[i] + cut(n - i))
+    return q
+print(cut(15))
+
+
+def matrix_min(a, b):
+    for i in range(1, len(a) + 1):
+        c[i] = [b[i], b[i + 1]]
+    print(c)
+
+a = range(1, 11)
+b = [12, 23, 21, 10, 100, 40, 5, 1, 30, 12, 3, 60]
+c = {}
