5386a29c8071138abf7f93d796428bbe195f6bdb
szw1223 <szw1223@outlook.com>
2020-09-01 14:20:49 -0400

    none

--- stack.py
+++ stack.py
@@ -1,24 +1,30 @@
 #leetcode 1021
 def removeOuterParentheses(S: str) -> str:
     stack = ''
-    flag = 0
     record = 0
     for i in range(len(S)):
         if S[i] == '(':
-            if record == 0 and flag == 0:
-                flag = 1
-                pass
-            else:
+            if record != 0 :
                 stack = stack + '('
             record += 1
         else:
-            if record == 1:
-                flag = 0
-                pass
-            else:
+            if record != 1:
                 stack = stack + ')'
+
             record -= 1
 
     return stack
+print(removeOuterParentheses('(()())()()'))
 
-print(removeOuterParentheses("(()())(())"))
+
+
+def runningSum(nums):
+    result = []
+    if len(nums) >= 1:
+        result.append(nums[0])
+    else:
+        return -1
+    for i in range(1, len(nums)):
+        result[i] = result[i - 1] + nums[i]
+    return result
+# print(runningSum([1,2,3,4]))
