2f4a4f6ab3506bf9268cd0367de6dbd129c63583
szw1223 <szw1223@outlook.com>
2020-09-02 02:27:56 -0400

    none

--- stack.py
+++ stack.py
@@ -14,7 +14,7 @@ def removeOuterParentheses(S: str) -> str:
             record -= 1
 
     return stack
-print(removeOuterParentheses('(()())()()'))
+# print(removeOuterParentheses('(()())()()'))
 
 
 
@@ -25,6 +25,41 @@ def runningSum(nums):
     else:
         return -1
     for i in range(1, len(nums)):
-        result[i] = result[i - 1] + nums[i]
+        result.append(result[i - 1] + nums[i])
     return result
-# print(runningSum([1,2,3,4]))
+
+print(runningSum([1,2,3,4]))
+
+
+def isValid(s: str) -> bool:
+    S = ''
+
+    for i in s:
+        if i == '(':
+            S = S + '('
+        elif i == '[':
+            S = S + '['
+        elif i == '{':
+            S = S + '{'
+        elif i == ')':
+            if len(S) <= 0:
+                return False
+            if S[len(S) - 1] == '(':
+                S = S[:len(S) - 1]
+        elif i == ']':
+            if len(S) <= 0:
+                return False
+            if S[len(S) - 1] == '[':
+                S = S[:len(S) - 1]
+        elif i == '}':
+            if len(S) <= 0:
+                return False
+            if S[len(S) - 1] == '{':
+                S = S[:len(S) - 1]
+        if len(S) < 0:
+            return False
+    if len(S) == 0:
+        return True
+    else:
+        return False
+print(isValid("(])"))
